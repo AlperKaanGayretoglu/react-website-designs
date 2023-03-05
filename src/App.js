24f37f4167392a23910d9b43494b65db0f581f6e
Alper Kaan Gayretoglu <alperkaangayretoglu@gmail.com>
2023-03-05 15:22:57 +0300

    Updated the App so that it is now have routes that point at the projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import BasicRegister from "./pages/p1-basic-register";
+import AdvancedRegister from "./pages/p2-advanced-register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/basic-register" element={<BasicRegister />} />
+      <Route path="/advanced-register" element={<AdvancedRegister />} />
+    </Routes>
+  );
+}
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Links</h1>
+      <nav>
+        <Link to="/basic-register">Basic Register</Link>
+        <Link to="/advanced-register">Advanced Register</Link>
+      </nav>
     </div>
   );
 }
